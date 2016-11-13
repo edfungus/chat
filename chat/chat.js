c909e419e8b2b56afbae7b34cb2f42b1236dfe2c
Edmund Fung <edfungus@gmail.com>
2016-11-12 16:14:16 -0800

    addClasses method

--- chat/chat.js
+++ chat/chat.js
@@ -1,18 +1,17 @@
 class Chat {
     constructor(container, classes, options) {
+        this.actions = new Actions();
         this.classes = classes != null ? classes : this.defaultClasses();
         this.options = options != null ? options : this.defaultOptions();
 
-        this.container = container;
-        this.container.classList.add(this.classes.container);            
+        this.container = this.addClasses(container, [this.classes.container]);
 
         this.setupInput();
         this.setupConversation();
     }
 
     setupInput() {
-        this.input = document.createElement("input");
-        this.input.classList.add(this.classes.input);
+        this.input = this.addClasses(document.createElement("input"), [this.classes.input]);
         this.container.appendChild(this.input);
         var that = this;
 
@@ -25,45 +24,58 @@ class Chat {
             var value = that.input.value.replace(/^(\s)|(\s+)$/g, ""); // removes whitespace before and after
             if (key === 13 && value != "") { 
                 that.addMessage("user", that.input.value);
-                 that.input.value = "";
+                that.input.value = "";
+
             }
         });
         
         setTimeout(function() {
-            that.input.classList.add(that.classes.inputDropIn);  
+            that.addClasses(that.input, [that.classes.inputDropIn]);  
             that.input.focus();               
         },1);
     }
 
     setupConversation() {
-        this.conversation = document.createElement("div");
-        this.conversation.classList.add(this.classes.conversation);
+        this.conversation = this.addClasses(document.createElement("div"), [this.classes.conversation]);
         this.container.appendChild(this.conversation);
 
         var top = document.createElement("div")
         top.style.height = this.options.conversationTopMargin;
         this.bottom = document.createElement("div")
         this.bottom.style.height = this.options.conversationBottomMargin;
-        this.bottom.classList.add("bottom-last-chat-element");
+        this.addClasses(this.bottom, ["bottom-last-chat-element"]);
+
         this.conversation.appendChild(top);
         this.conversation.appendChild(this.bottom);
     }
     
     addMessage(messageType, text) {
-        var message = document.createElement("div");
-        message.classList.add(this.classes.message, this.classes.messageTypes[messageType]);
-        var clearfix = document.createElement("div");
-        clearfix.classList.add(this.classes.clearfix);
+        var message = this.addClasses(document.createElement("div"), [this.classes.message, this.classes.messageTypes[messageType]]);
+        var clearfix = this.addClasses(document.createElement("div"), [this.classes.clearfix]);
 
         this.conversation.insertBefore(message, this.bottom);
         this.conversation.insertBefore(clearfix, this.bottom);
 
         var that = this;
         setTimeout(function() {
-            message.classList.add(that.classes.messageShow);
+            that.addClasses(message, [that.classes.messageShow]);
             message.innerHTML = text;            
             that.conversation.scrollTop = that.conversation.scrollHeight;                 
         },1);
+    }
+
+    processMessage(message) {
+        if(message[0] === ">") {
+            
+        }
+
+    }
+
+    addClasses(element, classList) {
+        for(var i = 0; i < classList.length; i++) {
+            element.classList.add(classList[i]);
+        }
+        return element;
     }
 
     defaultClasses() {
@@ -89,4 +101,8 @@ class Chat {
             defaultMessageType: "user"
         };
     } 
+}
+
+class Actions {
+    
 }
